a86d3673151dcfd456d9ce4da930548e0f6de530
Vickram Odero Josephat <vickramodero6@gmail.com>
2024-10-25 06:46:12 +0300

    review adding section functionality

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import {onMounted, ref, Ref} from 'vue'
-import {initFlowbite, initTabs} from 'flowbite'
+import {initFlowbite, initTabs, initModals} from 'flowbite'
 import {useRoute} from 'vue-router'
-import {Product, User} from "../models/Constants";
+import {Product, Review, User} from "../models/Constants";
 import {get, post} from "../boot";
 import {AxiosError, AxiosResponse} from "axios";
 import {formatPrice} from "../utils/formatPrice";
@@ -17,6 +17,41 @@ const user: Ref<User | null> = ref(null)
 const decodedToken = jwtDecode(localStorage.getItem('token') || '')
 const userId = decodedToken.sub
 const formattedDateTime = ref('')
+const errorMessage = ref('')
+
+const review: Ref<Review | null> = ref(null)
+const reviews: Ref<Review[] | null> = ref([])
+
+const reviewData = {
+  productId: productId,
+  userId: userId,
+  rating: 0,
+  commentTitle: '',
+  comment: '',
+  helpful: 0,
+}
+
+const addReview = () => {
+  post(`/reviews/${productId}`, review)
+      .then((response) => {
+        review.value = response.data.data
+      })
+      .catch((e: AxiosError) => {
+        console.log(e.response?.data.message)
+        errorMessage.value = e.response?.data.message
+      })
+}
+
+const fetchReviews = () => {
+  get(`/reviews/${productId}`)
+      .then((res: AxiosResponse) => {
+        reviews.value = res.data.data;
+      })
+      .catch((e) => {
+        console.log('Error fetching reviews:', e.response?.message);
+      })
+}
+
 
 const fetchProduct = () => {
   get(`/products/${productId}`)
@@ -61,6 +96,7 @@ const fetchUser = () => {
 onMounted(() => {
   initFlowbite()
   initTabs()
+  initModals()
   if (productId) {
     fetchProduct()
   }
@@ -185,7 +221,7 @@ onMounted(() => {
 
             <span class="text-gray-400">Give a note</span>
           </div>
-          <button class="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
+          <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="mt-4 px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700">
             Write a customer review
           </button>
         </div>
@@ -208,6 +244,62 @@ onMounted(() => {
           </select>
         </div>
       </div>
+      <!--      modal to add a review-->
+
+
+      <!-- Main modal -->
+      <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="relative p-4 w-full max-w-md max-h-full">
+          <!-- Modal content -->
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                Create New Product
+              </h3>
+              <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+              </button>
+            </div>
+            <!-- Modal body -->
+            <form class="p-4 md:p-5">
+              <div class="grid gap-4 mb-4 grid-cols-2">
+                <div class="col-span-2">
+                  <label for="commentTitle" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Comment Title</label>
+                  <input type="text" name="commentTitle" id="commentTitle" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                </div>
+                <div class="col-span-2 sm:col-span-1">
+                  <label for="rating" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rating</label>
+                  <input type="number" name="rating" id="rating" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="0" required="">
+                </div>
+                <div class="col-span-2 sm:col-span-1">
+                  <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                  <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                    <option selected="">Select category</option>
+                    <option value="TV">TV/Monitors</option>
+                    <option value="PC">PC</option>
+                    <option value="GA">Gaming/Console</option>
+                    <option value="PH">Phones</option>
+                  </select>
+                </div>
+                <div class="col-span-2">
+                  <label for="comment" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Comment</label>
+                  <textarea id="comment" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write comment here"></textarea>
+                </div>
+              </div>
+              <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                Add new product
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+
       <div class="border-t border-gray-300 w-full dark:border-gray-600 my-4"></div>
 
       <article>
